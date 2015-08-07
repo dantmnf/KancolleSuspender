34cec2044bd937c83d36bf12c5b20adc71db0abf
dantmnf <dantmnf2@gmail.com>
2015-08-07 23:55:23 +0800

    HiDPI scale support

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -144,8 +144,8 @@
             // 
             // MainForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
             this.AutoScrollMargin = new System.Drawing.Size(8, 8);
             this.AutoSize = true;
             this.ClientSize = new System.Drawing.Size(331, 121);
